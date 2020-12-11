a6ad6ae7d900da33c25e615a5ec49cec05ba15ab
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2020-12-11 12:31:08 +0900

    Add files via upload

--- test_line_tracer/test_line_tracer.ino
+++ test_line_tracer/test_line_tracer.ino
@@ -1,15 +1,27 @@
-#define SENSOR_THRESHOLD 200
+//#define SENSOR_THRESHOLD 200
+
+
 #define SENSOR_F_L B01
 #define SENSOR_F_R B10
 
-#define LT_MODULE_X0 22
-#define LT_MODULE_X1 23
+#define SENSOR_B_L B01
+#define SENOSR_B_R B10
+
+#define LT_MODULE_F0 22
+#define LT_MODULE_F1 23
+#define LT_MODULE_B0 24
+#define LT_MODULE_B1 25
+
+
 
 void init_line_tracer_modules()
 {
   Serial.begin(9600);
-  pinMode(LT_MODULE_X0, INPUT);//a0
-  pinMode(LT_MODULE_X1, INPUT);//a1
+  pinMode(LT_MODULE_F0, INPUT);//22
+  pinMode(LT_MODULE_F1, INPUT);//23
+  pinMode(LT_MODULE_B0, INPUT);//24
+  pinMode(LT_MODULE_B1, INPUT);//25
+  
 }
 
 void setup() {
@@ -23,6 +35,9 @@ void loop() {
   Serial.print("Sensor2 ->"); 
   Serial.println(digitalRead(LT_MODULE_X1));*/
   int onLine1=((digitalRead(LT_MODULE_X0)==1)?SENSOR_F_L:0)|((digitalRead(LT_MODULE_X1)==1)?SENSOR_F_R:0);
+  int onLine2=((digitalRead(LT_MODULE_B0)==1)?SENSOR_B_L:0)|((digitalRead(LT_MODULE_B1)==1)?SENSOR_B_R:0);
   Serial.print("onLine1->");
   Serial.println(onLine1);
+  Serial.print("onLine2->");
+  Serial.println(onLine2);
 }
