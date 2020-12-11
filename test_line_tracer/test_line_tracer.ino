98a5fbe14cf5fdf7729a320697709b289e9aeb46
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2020-12-11 13:09:53 +0900

    Add files via upload

--- test_line_tracer/test_line_tracer.ino
+++ test_line_tracer/test_line_tracer.ino
@@ -5,7 +5,7 @@
 #define SENSOR_F_R B10
 
 #define SENSOR_B_L B01
-#define SENOSR_B_R B10
+#define SENSOR_B_R B10
 
 #define LT_MODULE_F0 22
 #define LT_MODULE_F1 23
@@ -34,7 +34,7 @@ void loop() {
   Serial.println(digitalRead(LT_MODULE_X0));
   Serial.print("Sensor2 ->"); 
   Serial.println(digitalRead(LT_MODULE_X1));*/
-  int onLine1=((digitalRead(LT_MODULE_X0)==1)?SENSOR_F_L:0)|((digitalRead(LT_MODULE_X1)==1)?SENSOR_F_R:0);
+  int onLine1=((digitalRead(LT_MODULE_F0)==1)?SENSOR_F_L:0)|((digitalRead(LT_MODULE_F1)==1)?SENSOR_F_R:0);
   int onLine2=((digitalRead(LT_MODULE_B0)==1)?SENSOR_B_L:0)|((digitalRead(LT_MODULE_B1)==1)?SENSOR_B_R:0);
   Serial.print("onLine1->");
   Serial.println(onLine1);
